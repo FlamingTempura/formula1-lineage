966492389db81c2d0da2bf6572e9633f172b50cc
Peter West <peter.g.west@gmail.com>
2019-08-01 18:04:58 +0100

    more years

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* global d3 */
 'use strict';
 
-const idify = str => str.toLowerCase().replace(/[^a-z]+/, '-');
+const idify = str => 'i-' + str.toLowerCase().replace(/[^a-z0-9]+/, '-');
 
 const init = async () => {
 	const constructors = await d3.csv('data/constructors.csv');
@@ -13,12 +13,15 @@ const init = async () => {
 	const links = [];
 
 	for (const constructor of constructors) {
+		if (!constructor.lineage) { constructor.lineage = constructor.name; }
 		let lineage = lineages.find(d => d.name === constructor.lineage);
 		if (!lineage) {
-			lineage = { id: idify(constructor.lineage), name: constructor.lineage };
+			lineage = { id: idify(constructor.lineage), name: constructor.lineage, years: new Set(), constructors: [] };
 			lineages.push(lineage);
 		}
 		constructor.lineage = lineage;
+		constructor.years = new Set();
+		lineage.constructors.push(constructor);
 	}
 
 	for (const driver of drivers) {
@@ -34,6 +37,11 @@ const init = async () => {
 			
 			for (const constructorid of constructorids) {
 				const constructor = constructors.find(d => d.id === constructorid);
+				if (!constructor) {
+					throw new Error(`cannot find constructor ${constructorid}`);
+				}
+				constructor.years.add(year);
+				constructor.lineage.years.add(year);
 				const existing = entries.filter(d => d.constructor.lineage.id === constructor.lineage.id && d.year === year);
 				const entry = {
 					constructor,
@@ -57,9 +65,26 @@ const init = async () => {
 		}
 	}
 
-	const margin = { top: 70, right: 20, bottom: 40, left: 90 };
-	const width = 960 - margin.left - margin.right;
-	const height = 1000 - margin.top - margin.bottom;
+	const rows = [];
+	for (const lineage of lineages) {
+		let row = rows.findIndex(years => {
+			return ![...years].find(year => lineage.years.has(year));
+		});
+		if (row === -1) {
+			row = rows.length;
+			rows[row] = new Set();
+		}
+		const start = Math.min(...lineage.years);
+		const end = Math.max(...lineage.years);
+		for (let year = start; year <= end; year++) {
+			rows[row].add(year);
+		}
+		lineage.row = row;
+	}
+
+	const margin = { top: 70, right: 22, bottom: 40, left: 22 };
+	const width = 2060 - margin.left - margin.right;
+	const height = 1600 - margin.top - margin.bottom;
 
 	let selected = null;
 
@@ -68,12 +93,15 @@ const init = async () => {
 		.range([0, width]);
 
 	const y = d3.scaleBand()
-		.domain(lineages.map(c => c.id))
+		.domain(lineages.map(c => {
+			console.log(c); 
+			return c.row;
+		}))
 		.range([0, height]);
 
 	const update = () => {
-		path.attr('opacity', d => selected === d.from.driver.name || !selected ? 1 : 0.2);
-		entry.attr('opacity', d => selected === d.driver.name || !selected ? 1 : 0.2);
+		//path.attr('opacity', d => selected === d.from.driver.name || !selected ? 1 : 0.2);
+		//entry.attr('opacity', d => selected === d.driver.name || !selected ? 1 : 0.2);
 	};
 
 	const svg = d3.select('.container').append('svg')
@@ -107,11 +135,38 @@ const init = async () => {
 	const container = svg.append("g")
 		.attr("transform", `translate(${margin.left},${margin.top})`);
 
-	container.selectAll('.lineage')
+	// container.selectAll('.lineage')
+	// 	.data(lineages)
+	// 	.enter().append('text')
+	// 		.text(d => d.name)
+	// 		.attr('transform', d => `translate(-80, ${y(d.id) - 4})`);
+
+	const lineageg = container.selectAll('.lineage')
 		.data(lineages)
+		.enter().append('g')
+			.attr('transform', d => `translate(${x(Math.min(...d.years))}, ${y(d.row) - y.bandwidth() / 2})`);
+
+	lineageg.append('rect')
+		.attr('fill', d => d.constructors[0].color)
+		.attr('rx', 5)
+		.attr('ry', 5)
+		.attr('x', -20)
+		.attr('y', -6)
+		.attr('opacity', '0.3')
+		.attr('width', d => x(Math.max(...d.years)) - x(Math.min(...d.years)) + 40)
+		.attr('height', () => y.bandwidth() - 4);
+
+	// lineageg.append('text')
+	// 	.text(d => d.name)
+	// 	.attr('x', -14)
+	// 	.attr('y', 8);
+
+	lineageg.selectAll('text')
+		.data(d => d.constructors)
 		.enter().append('text')
-			.text(d => d.name)
-			.attr('transform', d => `translate(-80, ${y(d.id) - 4})`);
+			.attr('x', d => x(Math.min(...d.years)) - x(Math.min(...d.lineage.years)) - 14)
+			.attr('y', 8)
+			.text(d => d.name);
 
 	container.selectAll('.year')
 		.data(years)
@@ -126,9 +181,9 @@ const init = async () => {
 			.attr('class', d => `path n-${idify(d.from.name)}`)
 			.attr('d', d => {
 				let x1 = Math.round(x(d.from.year)),
-					y1 = Math.round(y(d.from.constructor.lineage.id) + (d.from.car - d.from.totalcars / 2) * 15),
+					y1 = Math.round(y(d.from.constructor.lineage.row) + (d.from.car - d.from.totalcars / 2) * 15),
 					x2 = Math.round(x(d.to.year)),
-					y2 = Math.round(y(d.to.constructor.lineage.id) + (d.to.car - d.to.totalcars / 2) * 15) + 0.001, // add tiny about to avoid straight lines (gradients don't work) https://stackoverflow.com/questions/21638169/svg-line-with-gradient-stroke-wont-display-straight/34687362
+					y2 = Math.round(y(d.to.constructor.lineage.row) + (d.to.car - d.to.totalcars / 2) * 15) + 0.001, // add tiny about to avoid straight lines (gradients don't work) https://stackoverflow.com/questions/21638169/svg-line-with-gradient-stroke-wont-display-straight/34687362
 					xc = Math.round(x1 + (x2 - x1) / 2),
 					yc = Math.round(y1 + (y2 - y1) / 2);
 				return `M${x1} ${y1} Q ${x1 + 22} ${y1}, ${xc} ${yc} T ${x2} ${y2}`;
@@ -147,7 +202,7 @@ const init = async () => {
 		.data(entries)
 		.enter().append('g')
 			.attr('class', 'entry')
-			.attr('transform', d => `translate(${x(d.year)},${y(d.constructor.lineage.id) + (d.car - d.totalcars / 2) * 15})`)
+			.attr('transform', d => `translate(${x(d.year)},${y(d.constructor.lineage.row) + (d.car - d.totalcars / 2) * 15})`)
 			.on('mousemove', d => {
 				selected = d.driver.name;
 				update();
